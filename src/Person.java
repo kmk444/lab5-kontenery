5fb79756ef0809bb3dcd0471496a95a659404db3
user <krystian.m342@gmail.com>
2025-04-26 22:12:41 +0200

    zad4

--- src/Person.java
+++ src/Person.java
@@ -1,18 +1,22 @@
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-public class Person {
+public class Person implements Comparable<Person> {
     private String first_name;
     private String last_name;
     private LocalDate birthDate;
 
-    private Set<Person> children = new HashSet<>(); // tworzymy new bo to nie jest inicjalizowane no i nie bierzemy go w konstruktorze
+    private SortedSet<Person> children = new TreeSet<>(); // tworzymy new bo to nie jest inicjalizowane no i nie bierzemy go w konstruktorze
 
     public Person(String first_name, String last_name, LocalDate birthDate) {
         this.first_name = first_name;
         this.last_name = last_name;
         this.birthDate = birthDate;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.birthDate.compareTo(o.birthDate);
     }
 
     public boolean adopt(Person child){
@@ -21,11 +25,7 @@ public class Person {
 
 
     public Person getYoungestChild(){
-        if (children.isEmpty()) return null;
-        Person youngest = (Person) children.toArray()[0];
-        for(Person child : children){
-            if (youngest.birthDate.compareTo(child.birthDate) < 0) youngest = child;
-        }
-        return youngest;
+        return children.last(); // da sie tego uzyc bo uzylismy Comparable i zmienilismy na SortedSet
     }
+
 }
